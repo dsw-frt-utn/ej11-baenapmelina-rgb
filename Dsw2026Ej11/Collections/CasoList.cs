d1d5de29f8aa3d5e4f0f5ad23a26536df6956804
Melina Baena <baenapmelina@gmail.com>
2026-05-20 10:16:24 -0300

    Desarrollé la clase CasoList con sus respectivos ejemplos

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+using System.Collections.Generic;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +11,46 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    //Crear un campo que represente una lista de alumnos (List<>)
+    List<Alumno> alumnos = new List<Alumno>();
+
+    //Incluir un método para agregar alumnos a la lista
+    public void AgregarAlumnos(Alumno alum)
+    {
+        alumnos.Add(alum);
+    }
+
+    //Incluir un método para retornar la lista
+    public List<Alumno> MostrarAlumnos()
+    {
+        return alumnos;
+    }
+
+    //Incluir un método para buscar un alumno por nombre
+    public bool BuscarAlumno (string nombre)
+    {
+
+        foreach(Alumno alu in alumnos)
+        {
+            if (alu.Nombre == nombre)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //Incluir un método para eliminar un alumno (debe recibir un alumno)
+    public void EliminarAlumno (Alumno alum)
+    {
+         alumnos.Remove(alum);
+    }
+
+    //Incluir un método para eliminar un alumno en una determinada posición de la lista
+    public void EliminarPorPosicion (int index)
+    {
+        if(index >= 0 && index < alumnos.Count)
+           alumnos.RemoveAt(index);  
+    }
 }
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,7 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,6 +14,75 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        Alumno alu1 = new Alumno(601, "Paula Lopez", 8.20);
+        Alumno alu2 = new Alumno(602, "Pedro Ortiz", 6.55);
+        Alumno alu3 = new Alumno(603, "Mariana Esposito",7.12 );
+
+        CasoList listaAlumnos = new CasoList();
+
+        //Agregar 3 alumnos a la lista
+        listaAlumnos.AgregarAlumnos(alu1);
+        listaAlumnos.AgregarAlumnos(alu2);
+        listaAlumnos.AgregarAlumnos(alu3);
+
+        //Listar por consola los alumnos
+        Console.WriteLine("== Listado de Alumnos ==");
+        var alumnos = listaAlumnos.MostrarAlumnos();
+
+        foreach(var alumno in alumnos)
+        {
+            Console.WriteLine(alumno);
+        }
+
+        //Buscar por nombre un alumno que exista y mostrar por consola
+        Console.WriteLine();
+        Console.WriteLine("== Búsqueda del Alumno Pedro Ortiz ==");
+        if (listaAlumnos.BuscarAlumno("Pedro Ortiz") == true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("El alumno que se buscaba fue encontrado");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("El alumno que se buscaba no existe");
+        }
+
+        //Buscar por nombre un alumno que no exista y mostrar por consola el texto "No existe"
+        Console.WriteLine();
+        Console.WriteLine("== Búsqueda del Alumno no existente ==");
+        if (listaAlumnos.BuscarAlumno("Mariela Sanchez") == true)
+        {
+            Console.WriteLine();
+            Console.WriteLine("El alumno que se buscaba fue encontrado");
+        }
+        else
+        {
+            Console.WriteLine();
+            Console.WriteLine("El alumno que se buscaba no existe");
+        }
+
+        //Eliminar un alumno y listar por consola los alumnos
+        Console.WriteLine();
+        Console.WriteLine("== Listado actualizado de alumnos ==");
+        Console.WriteLine();
+        listaAlumnos.EliminarAlumno(alu2);
+      
+        foreach (var alumno in alumnos)
+        {
+            Console.WriteLine(alumno);
+        }
+
+        //Eliminar el primer elemento de la lista y listar por consola los alumnos
+        Console.WriteLine();
+        Console.WriteLine("== Listado actualizado de alumnos luego de eliminar el primer elemento de la lista ==");
+        Console.WriteLine();
+        listaAlumnos.EliminarPorPosicion(0);
+     
+        foreach (var alumno in alumnos)
+        {
+            Console.WriteLine(alumno);
+        }
 
     }
 
